847464dc9d821df0be7b8fdb3d3894ca50afb95a
VK-NT2024 <157476607+VK-NT2024@users.noreply.github.com>
2024-11-08 17:56:48 +0100

    correction of QAM in modulation.py

--- ECCpython/Mapping/modulation.py
+++ ECCpython/Mapping/modulation.py
@@ -57,6 +57,106 @@ class Modulation(Mapping):
     def spectral_efficiency(self):
         return self.m
 
+    def rcosfilter(N, alpha, Ts, Fs):
+        """
+        Generates a raised cosine (RC) filter (FIR) impulse response.
+
+        Parameters
+        ----------
+        N : int
+            Length of the filter in samples.
+
+        alpha : float
+            Roll off factor (Valid values are [0, 1]).
+
+        Ts : float
+            Symbol period in seconds.
+
+        Fs : float
+            Sampling Rate in Hz.
+
+        Returns
+        -------
+
+        h_rc : 1-D ndarray (float)
+            Impulse response of the raised cosine filter.
+
+        time_idx : 1-D ndarray (float)
+            Array containing the time indices, in seconds, for the impulse response.
+        """
+
+        T_delta = 1 / float(Fs)
+        time_idx = ((np.arange(N) - N / 2)) * T_delta
+        sample_num = np.arange(N)
+        h_rc = np.zeros(N, dtype=float)
+
+        for x in sample_num:
+            t = (x - N / 2) * T_delta
+            if t == 0.0:
+                h_rc[x] = 1.0
+            elif alpha != 0 and t == Ts / (2 * alpha):
+                h_rc[x] = (np.pi / 4) * (np.sin(np.pi * t / Ts) / (np.pi * t / Ts))
+            elif alpha != 0 and t == -Ts / (2 * alpha):
+                h_rc[x] = (np.pi / 4) * (np.sin(np.pi * t / Ts) / (np.pi * t / Ts))
+            else:
+                h_rc[x] = (np.sin(np.pi * t / Ts) / (np.pi * t / Ts)) * \
+                          (np.cos(np.pi * alpha * t / Ts) / (1 - (((2 * alpha * t) / Ts) * ((2 * alpha * t) / Ts))))
+
+        return time_idx, h_rc
+
+    def rrcosfilter(N, alpha, Ts, Fs):
+        """
+        Generates a root raised cosine (RRC) filter (FIR) impulse response.
+
+        Parameters
+        ----------
+        N : int
+            Length of the filter in samples.
+
+        alpha : float
+            Roll off factor (Valid values are [0, 1]).
+
+        Ts : float
+            Symbol period in seconds.
+
+        Fs : float
+            Sampling Rate in Hz.
+
+        Returns
+        ---------
+
+        h_rrc : 1-D ndarray of floats
+            Impulse response of the root raised cosine filter.
+
+        time_idx : 1-D ndarray of floats
+            Array containing the time indices, in seconds, for
+            the impulse response.
+        """
+
+        T_delta = 1 / float(Fs)
+        time_idx = ((np.arange(N) - N / 2)) * T_delta
+        sample_num = np.arange(N)
+        h_rrc = np.zeros(N, dtype=float)
+
+        for x in sample_num:
+            t = (x - N / 2) * T_delta
+            if t == 0.0:
+                h_rrc[x] = 1.0 - alpha + (4 * alpha / np.pi)
+            elif alpha != 0 and t == Ts / (4 * alpha):
+                h_rrc[x] = (alpha / np.sqrt(2)) * (((1 + 2 / np.pi) * \
+                                                    (np.sin(np.pi / (4 * alpha)))) + (
+                                                               (1 - 2 / np.pi) * (np.cos(np.pi / (4 * alpha)))))
+            elif alpha != 0 and t == -Ts / (4 * alpha):
+                h_rrc[x] = (alpha / np.sqrt(2)) * (((1 + 2 / np.pi) * \
+                                                    (np.sin(np.pi / (4 * alpha)))) + (
+                                                               (1 - 2 / np.pi) * (np.cos(np.pi / (4 * alpha)))))
+            else:
+                h_rrc[x] = (np.sin(np.pi * t * (1 - alpha) / Ts) + \
+                            4 * alpha * (t / Ts) * np.cos(np.pi * t * (1 + alpha) / Ts)) / \
+                           (np.pi * t * (1 - (4 * alpha * t / Ts) * (4 * alpha * t / Ts)) / Ts)
+
+        return time_idx, h_rrc
+
     @property
     def constellation(self):
         import matplotlib.pyplot as plt
@@ -156,9 +256,8 @@ class Modulation(Mapping):
     def QAM(self):
         """
         """
-        Re, Im = np.meshgrid(np.arange(1, self.m + 1), np.arange(self.m, 0, -1))
-        Re = Re - np.mean(Re)
-        Im = Im - np.mean(Im)
+        sqrtM = np.sqrt(self.M)
+        Re, Im = np.meshgrid(np.arange(1-sqrtM, sqrtM,2), np.arange(1-sqrtM, sqrtM,2))
         return np.array((Re + 1j * Im).ravel())
 
     def simulate(self, info_length, trials=100, SNRdB_min=-1, SNRdB_max=30, SNRdB_step=1, algorithm="approximation"):
